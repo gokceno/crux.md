57d93c5dfad57fc892fc79b779366f994c99e3d8
Gokcen Ogutcu <gokcen@brewww.com>
2023-08-20 13:09:51 +0300

    feat: source.filesystem moved to filesystem

--- apps/api/routes/schema.js
+++ apps/api/routes/schema.js
@@ -1,12 +1,12 @@
 import fs from 'fs/promises'
 import { GraphQLSchema, GraphQLObjectType } from 'graphql';
 import { Bucket } from '@crux/bucket';
-import { Source } from '@crux/source';
+import { FileSystem } from '@crux/bucket-source-filesystem';
 import { Resolvers } from '@crux/graphql-api-resolvers';
 import { transform } from '@crux/graphql-api';
 
 const bucket = Bucket().load({
-  source: Source().FileSystem({ bucketPath: '../../samples/bucket' })
+  source: FileSystem({ bucketPath: '../../samples/bucket' })
 });
 
 const manifest = await bucket.manifest();
