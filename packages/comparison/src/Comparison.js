e5bb1af4143b97e08fb9f92982193e67318fdef7
Gokcen Ogutcu <gokcen@brewww.com>
2023-08-20 13:19:55 +0300

    chore

--- packages/comparison/src/Comparison.js
+++ packages/comparison/src/Comparison.js
@@ -1,8 +1,8 @@
-export const _eq = (iValue, cValue) => iValue == cValue;
-export const _neq = (iValue, cValue) => iValue != cValue;
-export const _null = (iValue, cValue) => iValue === null || iValue === undefined || iValue.length === 0;
-export const _nnull = (iValue, cValue) => iValue !== null && iValue !== undefined && iValue.length !== 0;
-export const _gte = (iValue, cValue) => +iValue >= +cValue;
-export const _lte = (iValue, cValue) => +iValue <= +cValue;
-export const _gt = (iValue, cValue) => +iValue > +cValue;
-export const _lt = (iValue, cValue) => +iValue < +cValue;
+export const _eq = (a, b) => a == b;
+export const _neq = (a, b) => a != b;
+export const _null = (a, b) => a === null || a === undefined || a.length === 0;
+export const _nnull = (a, b) => a !== null && a !== undefined && a.length !== 0;
+export const _gte = (a, b) => +a >= +b;
+export const _lte = (a, b) => +a <= +b;
+export const _gt = (a, b) => +a > +b;
+export const _lt = (a, b) => +a < +b;
