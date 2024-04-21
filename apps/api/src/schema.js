6d8812b0d211af7d8e05c988484aa6a2d4cb9e00
Gokcen Ogutcu <gokcen@brewww.com>
2024-04-21 10:27:29 +0300

    feat: added access key and secret key generation to schema

--- apps/api/src/schema.js
+++ apps/api/src/schema.js
@@ -1,12 +1,13 @@
 import { sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { sql } from 'drizzle-orm';
 import { createId } from '@paralleldrive/cuid2';
+import { randomBytes } from 'node:crypto';
 
 export const buckets = sqliteTable('buckets', {
   id: text('id').notNull().$defaultFn(() => createId()),
   source: text('source').notNull(),
   cache: text('cache').notNull(),
-  accessKey: text('access_key').notNull(),
-  secretKey: text('secret_key').notNull(),
+  accessKey: text('access_key').notNull().$defaultFn(() => randomBytes(64).toString('base64url')),
+  secretKey: text('secret_key').notNull().$defaultFn(() => randomBytes(64).toString('base64url')),
   createdAt: text('created_at').notNull().default(sql`CURRENT_TIMESTAMP`),
 });
